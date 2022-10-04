a904b4691d391c96b02a5f7715f832a3f8d4c23c
Matthew Suddaby <msuddaby2@nait.ca>
2022-10-04 08:29:37 -0600

    jefffff

--- ScratchPad3/Program.cs
+++ ScratchPad3/Program.cs
@@ -10,7 +10,7 @@ namespace ScratchPad3
         static async Task Main(string[] args)
         {
             HttpService service = new HttpService();
-
+            Console.WriteLine("jefffff");
             var result = await service.Get<PostDTO>($"https://beta.a2b2.org/api/posts/20");
 
             Console.WriteLine(result.Response.ImageURL);
